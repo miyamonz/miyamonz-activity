e895197a2bb8f2a83df59295c5c8d64feb94a7ef
Koichi Miyamoto <m5813k@gmail.com>
2018-12-23 10:33:43 +0900

    env-cmd

---
+++ app.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+async function fetch(date) {
+  const config = {
+    baseURL: "https://api.fitbit.com",
+    url: `/1.2/user/-/sleep/date/${date}.json`,
+    method: "get",
+    headers: {
+      //TOKENの有効期限注意
+      Authorization: `Bearer ${process.env.FITBIT_ACCESS_TOKEN}`
+    }
+  };
+
+  const res = await axios.request(config);
+  return res.data;
+}
+
+const data = fetch("2018-12-20");
+data.then(console.log);
