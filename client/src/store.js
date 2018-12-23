2a6b936cf70cfce948704981580179e31117d54e
Koichi Miyamoto <m5813k@gmail.com>
2018-12-23 23:04:28 +0900

    api

--- client/src/store.js
+++ client/src/store.js
@@ -11,7 +11,7 @@ new Vue({
 class Store {
   constructor() {}
   async fetch() {
-    const url = process.env.VUE_APP_API_ENDPOINT;
+    const url = process.env.VUE_APP_API_ENDPOINT || "/api";
     console.log(url);
     const json = await fetch(url, { mode: "cors" }).then(res => res.json());
     console.log(json);
