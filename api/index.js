19955aba9551fd57ed0580299d045129006b2495
Koichi Miyamoto <m5813k@gmail.com>
2018-12-23 19:57:20 +0900

    10日ぶんとるようにした

--- api/index.js
+++ api/index.js
@@ -2,13 +2,27 @@ const Koa = require("koa");
 const cors = require("@koa/cors");
 const app = new Koa();
 
+const { DateTime } = require("luxon");
+
 const fetch = require("./fetch.js");
-const url = date => `/1.2/user/-/sleep/date/${date}.json`;
+const dateApi = date => `/1.2/user/-/sleep/date/${date}.json`;
+const listApi = params =>
+  `/1.2/user/-/sleep/list.json?${Object.entries(params)
+    .map(([key, val]) => key + "=" + val)
+    .reduce((acc, p) => acc + "&" + p)}`;
 
 app.use(cors({ origin: "*" }));
 app.use(async ctx => {
-  const date = "2018-12-22";
-  ctx.body = await fetch(url(date));
+  const date = DateTime.local()
+    .plus({ days: -10 })
+    .toFormat("yyyy-MM-dd");
+  console.log(date);
+
+  const url = listApi({ afterDate: date, offset: 0, limit: 10, sort: "desc" });
+  console.log({ url });
+  // const url = dateApi(date);
+  ctx.body = await fetch(url);
 });
 
+console.log("listen on ", process.env.PORT);
 app.listen(process.env.PORT);
