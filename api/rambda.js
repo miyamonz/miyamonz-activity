414a6633be4db4fbded36acce8a3d22c353718a7
Koichi Miyamoto <m5813k@gmail.com>
2018-12-23 23:11:24 +0900

    30日にした

--- api/rambda.js
+++ api/rambda.js
@@ -3,11 +3,11 @@ const { fetch, listApi } = require("./fetch.js");
 
 module.exports = (req, res) => {
   const date = DateTime.local()
-    .plus({ days: -10 })
+    .plus({ days: -30 })
     .toFormat("yyyy-MM-dd");
   console.log(date);
 
-  const url = listApi({ afterDate: date, offset: 0, limit: 10, sort: "desc" });
+  const url = listApi({ afterDate: date, offset: 0, limit: 30, sort: "desc" });
   console.log({ url });
   fetch(url).then(json => res.end(JSON.stringify(json)));
 };
